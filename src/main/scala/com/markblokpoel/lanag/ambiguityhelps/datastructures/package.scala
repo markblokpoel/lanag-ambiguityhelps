41904e0f5d07e1528314462c92f6d33775179447
Mark Blokpoel <mark.blokpoel@gmail.com>
2019-11-06 14:46:54 +0100

    Updated documentation and added notebooks

---
+++ src/main/scala/com/markblokpoel/lanag/ambiguityhelps/datastructures/package.scala
@@ -0,0 +1,8 @@
+package com.markblokpoel.lanag.ambiguityhelps
+
+/**
+  * This package contains case classes for storing data of the rational speech act simulations.
+  */
+package object datastructures {
+
+}
---
+++ src/main/scala/com/markblokpoel/lanag/ambiguityhelps/experiments/structured/package.scala
@@ -0,0 +1,10 @@
+package com.markblokpoel.lanag.ambiguityhelps.experiments
+
+/**
+  * This package contains all the necessary classes for running a Rational Speech Act simulation where
+  * agent pairs are generated using a strategy that generates binary string representations for referents and
+  * signals.
+  */
+package object structured {
+
+}
